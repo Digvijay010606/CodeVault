0869892c14e581f715fe648d589ce78fa59786a9
Digvijay010606 <digvijay010606@gmail.com>
2026-08-13 18:01:59 +0530

    updated

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import pathlib
+
 print("hello welcome to CodeVault")
+
+target_directory = pathlib.Path(input("Enter the directory: "))
+
+for item in target_directory.rglob("*.py"):
+    if item.is_file():
+        print(f"file: {item}")
+        print("----------------- Reading content:")
+        try:
+            content = item.read_text(encoding = 'utf-8')
+            print(content)
+        except Exception as e:
+            print("Could not read content")
+
+    elif item.is_dir():
+        print(f"directory: {item}")
+
