6d174ac59613a4f1e3bf1e5767e3355d6dba1d52
Digvijay010606 <digvijay010606@gmail.com>
2026-08-16 14:52:28 +0530

    redesigned the scanner.py

--- scanner.py
+++ scanner.py
@@ -1,14 +1,56 @@
-import pathlib
+from pathlib import Path
+
+SUPPORTED_EXTENSIONS = {
+    ".py",
+    ".c",
+    ".cpp",
+    ".js",
+    ".java",
+    ".html",
+    ".css",
+    ".h",
+    ".hpp"
+}
 
 def scan_directory(target_directory):
-    for item in target_directory.rglob("*.py"):
-        if item.is_dir():
-            return "Directory: {item}"
-        elif item.is_file():
-            
-            try:
-                content = item.read_text(encoding = 'utf-8')
-                return item,content
-            except Exception as e:
-                print("Cannot read the content of file")
 
+    target_directory = Path(target_directory)
+
+    if not target_directory.exists():
+        raise FileNotFoundError(
+            print(f"Directory does not exists: {target_directory}")
+        )
+
+    if not target_directory.is_dir():
+        raise NotADirectoryError(
+            print(f"Not a Directory: {target_directory}")
+        )
+
+    files = []
+
+    for file in target_directory.rglob("*"):
+
+        if not file.is_file():
+            continue
+        
+        if file.suffix.lower() not in SUPPORTED_EXTENSIONS:
+            continue
+
+        if any(part in {"__pycache__", ".git", "venv", ".venv"} for part in file.parts):
+            continue
+
+        try:
+            content = file.read_text(
+                encoding = 'utf-8',
+                errors = 'ignore'
+            )
+
+            files.append({
+                "path" : file,
+                "content" : content
+            })
+
+        except OSError as error:
+            print(f"Could not read {file}: {errro}")
+
+    return files
